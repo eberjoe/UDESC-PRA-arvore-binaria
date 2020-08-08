e34e11865ec7348f55286f4ae2b7847a041e7b78
Eber <eberjoe@gmail.com>
2020-08-07 22:34:46 -0300

    Exercícios

---
+++ 03/arvore.c
@@ -0,0 +1,87 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "arvore.h"
+
+void inicializa_arvore(Arvore *arvore) {
+    arvore->raiz = NULL;
+}
+
+int arvore_vazia(Arvore *arvore) {
+    return arvore->raiz == NULL;
+}
+
+No* adiciona_no(Arvore *arvore, No *pai, int valor) {
+    No *no = malloc(sizeof(No));
+    no->pai = pai;
+    no->esquerda = NULL;
+    no->direita = NULL;
+    no->valor = valor;
+    if (pai == NULL) {
+        arvore->raiz = no;
+    }
+    return no;
+}
+
+void remove_no(Arvore *arvore, No *no) {
+    if (no->esquerda != NULL) {
+        remove_no(arvore, no->esquerda); 
+    }
+    if (no->direita != NULL) {
+        remove_no(arvore, no->direita);
+    }
+    if (no->pai == NULL) {
+        arvore->raiz = NULL;
+    } else {
+        if (no->pai->esquerda == no) {
+            no->pai->esquerda = NULL;
+        } else {
+            no->pai->direita = NULL;
+        }
+    }
+    free(no);
+}
+
+void percorrer_InOrder(No *no, void (*callback) (int)) {
+    if (no != NULL) {
+        percorrer_InOrder(no->esquerda, callback);
+        callback(no->valor);
+        percorrer_InOrder(no->direita, callback);
+    }
+}
+
+void percorrer_PreOrder(No *no, void (*callback) (int)) {
+    if (no != NULL) {
+        callback(no->valor);
+        percorrer_PreOrder(no->esquerda, callback);
+        percorrer_PreOrder(no->direita, callback);
+    }
+}
+
+void percorrer_PosOrder(No *no, void (*callback) (int)) {
+    if (no != NULL) {
+        percorrer_PosOrder(no->esquerda, callback);
+        percorrer_PosOrder(no->direita, callback);
+        callback(no->valor);
+    }
+}
+
+int custo_busca(Arvore *arvore, int chave) {
+    int custo = 1;
+    if (!arvore_vazia(arvore)) {
+        No *no = arvore->raiz;
+        if (no->valor == chave)
+            return custo;
+        while ((no->esquerda || no->direita) && no->valor != chave) {
+            custo++;
+            if (chave > no->valor && no->direita)
+                no = no->direita;
+            else if (no->esquerda)
+                no = no->esquerda;
+        }
+        if (no->valor == chave)
+            return custo;
+        else
+            return -1;
+    }
+    return 0;
+}
---
+++ 03/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "arvore.h"
+
+void mostrar(int valor);
+
+int main() {
+    Arvore a;
+    inicializa_arvore(&a);
+
+    No* no4 = adiciona_no(&a, NULL, 4);
+    
+    No* no2 = adiciona_no(&a, no4, 2);
+    no4->esquerda = no2;
+
+    No* no8 = adiciona_no(&a, no4, 8);
+    no4->direita = no8;
+
+    No* no1 = adiciona_no(&a, no2, 1);
+    no2->esquerda = no1;
+
+    No* no3 = adiciona_no(&a, no2, 3);
+    no2->direita = no3;
+
+    No* no6 = adiciona_no(&a, no8, 6);
+    no8->esquerda = no6;
+
+    No* no9 = adiciona_no(&a, no8, 9);
+    no8->direita = no9;
+
+    No* no5 = adiciona_no(&a, no6, 5);
+    no6->esquerda = no5;
+
+    No* no7 = adiciona_no(&a, no6, 7);
+    no6->direita = no7;
+
+    int in;
+    printf("Entre a chave a ser buscada: ");
+    scanf("%d", &in);
+    printf("O custo da busca é %d \n", custo_busca(&a, in));
+
+    return 1;
+}
+
+void mostrar(int valor) {
+    printf("%d ", valor);
+}
